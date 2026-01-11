4158a2cdd702a0962772d15ca72b5edee90c0d3e
Вадим Дабижа <dabizha.vn@phystech.edu>
2026-01-11 23:51:00 +0300

    some fixes

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,3 @@
 from .bot import main
-from .api import search_vk_video, search_all, fetch_movie_by_query
-__all__ = ["main", "search_vk_video", "search_all", "fetch_movie_by_query"]
+
+__all__ = ["main"]
